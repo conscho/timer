22ad2eb24d65eb5d38359cca21441d74a17e8dc9
Constantin Scholl <constantin@scholl.im>
2016-10-10 14:53:39 -0400

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python hello.py $PORT
+web: python hello.py
--- hello.py
+++ hello.py
@@ -1,11 +1,7 @@
 from flask import Flask
 import time
-import sys
 app = Flask(__name__)
 
 @app.route('/')
 def timer():
     return str(time.time() * 1000)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=sys.argv[1])
