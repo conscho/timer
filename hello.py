229155549765e023bf44384210165248d705e599
Constantin Scholl <constantin@scholl.im>
2016-10-12 11:24:00 -0400

    change to int

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def timer():
-    return str(time.time() * 1000)
+    return str(int(time.time()))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=sys.argv[1])
